8e5af417fe0572392f25797672b4f4657a1fdf1b
mirellanovroch <mirella_novroch@outlook.com>
2025-07-21 17:58:06 -0300

    docs: Adiciona comentários ao código.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,18 @@
+// Seleciona a lista onde os Pokémons serão exibidos
 const pokemonList = document.getElementById('pokemonList')
+// Seleciona o botão "Carregar mais"
 const loadMoreButton = document.getElementById('loadMoreButton')
-
+// Define o total máximo de Pokémons que serão carregados
 const maxRecords = 493
 const limit = 12
+// Define o ponto de início do carregamento (inicialmente 0)
 let offset = 0;
 
-
-
+// Função que carrega os Pokémons e os insere no HTML
 function loadPokemonItens(offset,limit) {
+    // Chama a API para buscar os Pokémons
     pokeApi.getPokemons(offset,limit).then((pokemons = []) => {
+    // Gera o HTML de cada Pokémon
     const newHtml = pokemons.map((pokemon) => `
          <li class="pokemon ${pokemon.type}">
                  <span class="number">#${pokemon.number}</span>
@@ -23,24 +27,29 @@ function loadPokemonItens(offset,limit) {
                          alt="${pokemon.name}">
                  </div>
              </li>
-         `).join('')
+         `).join('') // Junta todos os cards em uma única string
          pokemonList.innerHTML+= newHtml
  })
 
 }
-
+// Quando o botão "Carregar mais" for clicado
 loadPokemonItens(offset, limit)
 
+// Quando o botão "Carregar mais" for clicado
 loadMoreButton.addEventListener('click', () => {
     offset += limit
     const qtRecordWithNextPage = offset + limit
 
+     // Se a próxima página ultrapassar o total máximo de Pokémons
     if (qtRecordWithNextPage >= maxRecords) {
+
         const newLimit = maxRecords - offset
+        // Remove o botão após atingir o limite máximo
         loadPokemonItens(offset, newLimit)
 
         loadMoreButton.parentElement.removeChild(loadMoreButton)
     }else {
+        // Se ainda não chegou no limite, continua carregando normalmente
         loadPokemonItens(offset,limit)
     }
 
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,37 +1,39 @@
 const pokeApi = {}
 
+// Converte os dados da API para o formato usado no nosso app
 function convertPokeApiDetailToPokemon(pokeDetail) {
     const pokemon = new Pokemon()
     pokemon.number = pokeDetail.id
     pokemon.name = pokeDetail.name
 
+    // Pega os tipos do Pokémon (ex: água, fogo)
     const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
     const [type] = types
 
     pokemon.types = types
     pokemon.type = type
 
+    // Define a imagem do Pokémon (versão "dream_world")
     pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
 
     return pokemon
 }
 
-
+// Pega os detalhes de um Pokémon usando a URL dele
 pokeApi.getPokemonDetail = (pokemon) => {
     return fetch(pokemon.url)
-    .then((response) => response.json())
-    .then(convertPokeApiDetailToPokemon)
+        .then((response) => response.json())
+        .then(convertPokeApiDetailToPokemon)
 
 }
-
+// Busca uma lista de Pokémons com base no limite e no deslocamento
 pokeApi.getPokemons = (offset = 0, limit = 5) => {
-    // const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&mit=${limit}`
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
-    return fetch(url)
-        .then((response) => response.json())
-        .then((jsonBody) => jsonBody.results) 
-        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
-        .then((detailRequests) => Promise.all(detailRequests))
-        .then((pokemonDetails) => pokemonDetails)
+    return fetch(url) // Faz a requisição da lista de Pokémons
+        .then((response) => response.json()) // Converte a resposta para JSON
+        .then((jsonBody) => jsonBody.results) // Pega apenas o array de Pokémons
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail)) // Para cada Pokémon, pega os detalhes
+        .then((detailRequests) => Promise.all(detailRequests)) // Espera todas as requisições terminarem
+        .then((pokemonDetails) => pokemonDetails) // Retorna os dados completos dos Pokémons
 }
